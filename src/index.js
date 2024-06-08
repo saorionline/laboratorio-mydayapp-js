8a77ec54f9a86d1635d47d8cac4b32822f5046d3
Saori Isabel Tovar Niño <saorisabeltovar@gmail.com>
2024-06-08 10:02:10 -0500

    Redone

--- src/index.js
+++ src/index.js
@@ -1,54 +1,64 @@
 import "./css/base.css";
-import "./css/card.css";
-
-import { 
-  renderAllTasks, 
-} from './js/utils.js';
-
-import { 
-  updateCount, 
-  filterReady,
-}
-from "./js/second-update-tasks.js";
-
-import { 
-  checkTasks,
-  completedCount, 
-}
-from "./js/third-check-tasks.js";
-
 import {
-  loadAllToDos,
-  loadNewTask,
-  loadFilter,
-  loadClearDone,
-}
-from "./js/closed-cycle.js";
+  sayHello,
+  checkItems,
+  addTask,
+  clearTasks,
+  filterTasks,
+} from "./js/utils";
 
-function init() {
-  //Render the current tasks
-  renderAllTasks();
-  // Updating the pending tasks counter
-  updateCount();
-  // Checking if any filter should be applied when the application loads
-  filterReady();
-  // Checking if there are tasks available in order to show the footer
-  checkTasks();
-  // Checking if there are completed tasks in order to show the "clear completed" button
-  completedCount();  
+console.log(sayHello("Hello"));
 
-  // Starting event listeners 
-  loadAllToDos();
-  loadNewTask();
-  loadFilter();
-  loadClearDone();
-}
-init();
- 
+document.onload = checkItems();
 
-function clearLocalStorage() {
-  localStorage.removeItem("mydayapp-js");
+const input = document.querySelector(".new-todo");
+
+if (input) {
+  input.addEventListener("keyup", (event) => {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      if (event.target.value) {
+        addTask(event.target.value.trim());
+        input.value = "";
+      }
+    }
+  });
 }
 
-// Call the function whenever you want to reset the tasks
-clearLocalStorage();
+const cleanButton = document.querySelector(".clear-completed");
+
+if (cleanButton) {
+  cleanButton.addEventListener("click", () => {
+    clearTasks();
+  });
+}
+
+const filterButtons = document.querySelectorAll(".filters li a");
+
+filterButtons.forEach((button) => {
+  const type = button.getAttribute("href").substring(2);
+  button.addEventListener("click", () => {
+    if (!button.classList.contains("selected")) {
+      const activeButton = document.querySelector(".selected");
+      if (activeButton) {
+        activeButton.className = "";
+      }
+      button.className = "selected";
+      filterTasks(type);
+    }
+  });
+});
+
+window.addEventListener("hashchange", function () {
+  const hash = window.location.hash || "#/"; // Obtener el fragmento de la URL
+  const type = hash.substring(2);
+  const activeButton = document.querySelector(".selected");
+  if (activeButton) {
+    activeButton.className = "";
+  }
+  const link = document.querySelector(`a[href="${hash}"]`);
+  link.className = "selected";
+  filterTasks(type);
+  // Aquí puedes poner tu lógica para manejar el cambio de ruta
+  console.log("La ruta ha cambiado a", hash);
+});
--- src/js/utils.js
+++ src/js/utils.js
@@ -1,90 +1,250 @@
-import { updateTasksList } from "./second-update-tasks.js";
+export const sayHello = (text) => {
+  return text;
+};
 
-
-export function getTasks() {
-  /** @type {Array<Task>}*/
-  let tasks;
-
-  // Checking if the tasks already exist in localstorage if not creating a new default list
-  const stringTasks = localStorage.getItem("mydayapp-js")
-  if (stringTasks != undefined) {
-    tasks = JSON.parse(stringTasks)
+export const checkItems = () => {
+  const items = getStorage();
+  if (!items || items.length === 0) {
+    document.addEventListener("DOMContentLoaded", hideContent);
+    showClearButton(items || []);
   } else {
-    tasks = [];
-     localStorage.setItem("mydayapp-js", JSON.stringify([]));
+    renderItems(items);
+    updateTotalTasks(items);
+    showClearButton(items);
+  }
+};
+
+export const addTask = (value) => {
+  const items = getStorage();
+  const container = document.querySelector(".todo-list");
+  const fragment = document.createDocumentFragment();
+  const newItem = {
+    id: "task1",
+    title: value,
+    completed: false,
+  };
+  renderItem(newItem, fragment);
+  container.appendChild(fragment);
+  if (!items || items.length === 0) {
+    localStorage.setItem("mydayapp-js", JSON.stringify([newItem]));
+    showContent();
+    updateTotalTasks([newItem]);
+    showClearButton([newItem]);
+  } else {
+    newItem.id = `task${items.length + 1}`;
+    localStorage.setItem("mydayapp-js", JSON.stringify([...items, newItem]));
+    updateTotalTasks([...items, newItem]);
+    showClearButton([...items, newItem]);
+  }
+};
+
+export const clearTasks = () => {
+  const storage = localStorage.getItem("mydayapp-js");
+  const items = storage ? JSON.parse(storage) : [];
+  const incompleteItems = items.filter((item) => !item.completed);
+  localStorage.setItem("mydayapp-js", JSON.stringify(incompleteItems));
+  const list = document.querySelector(".todo-list");
+  if (list) {
+    list.innerHTML = "";
+  }
+  if (incompleteItems.length === 0) {
+    hideContent();
+  } else {
+    renderItems(incompleteItems);
+  }
+  updateTotalTasks(incompleteItems);
+  showClearButton([]);
+};
+
+export const filterTasks = (type) => {
+  const storage = localStorage.getItem("mydayapp-js");
+  const items = storage ? JSON.parse(storage) : [];
+  let filterItems = items;
+  if (type === "pending") {
+    filterItems = items.filter((item) => !item.completed);
+  } else if (type === "completed") {
+    filterItems = items.filter((item) => item.completed);
+  }
+  const list = document.querySelector(".todo-list");
+  if (list) {
+    list.innerHTML = "";
+  }
+  renderItems(filterItems);
+  updateTotalTasks(filterItems);
+};
+
+const hideContent = () => {
+  //Función para esconder las barras
+  const mainElement = document.querySelector("#main");
+  const footerElement = document.querySelector("#footer");
+  if (mainElement) {
+    mainElement.style.display = "none";
+  }
+  if (footerElement) {
+    footerElement.style.display = "none";
   }
 
-  return tasks;
-}
+  document.removeEventListener("DOMContentLoaded", hideContent);
+};
 
-// Render a single task
-export function renderTask(task) { //task
-  const textBox = document.querySelector('.todo-list'); //taskContainer
+const showContent = () => {
+  //Función para mostrar las barras
+  const mainElement = document.querySelector("#main");
+  const footerElement = document.querySelector("#footer");
+  if (mainElement) {
+    mainElement.style.display = "block";
+  }
+  if (footerElement) {
+    footerElement.style.display = "block";
+  }
+};
 
-  let textHTML = `
-    <li class="${task.completed ? "completed" :""}" 
-    data-taskid=${task.id}
-    data-task-state="${task.completed === false ? "pending": "completed"}">
-      <div class="view">
-        <input class="toggle" type="checkbox" ${task.completed ? "checked":""}>
-        <label>${task.text}</label>
-        <button class="destroy"></button>
-      </diV> 
-      <input class="edit" value="${task.text}" /> 
-    </li>
-  `
-  textBox.insertAdjacentHTML("beforeend", textHTML);
+const getStorage = () => {
+  const items = localStorage.getItem("mydayapp-js");
+  return items ? JSON.parse(items) : null;
+};
 
-}
-// Render all tasks
+const renderItems = (items) => {
+  const container = document.querySelector(".todo-list");
+  const fragment = document.createDocumentFragment();
 
-export function renderAllTasks(){
-  const tasks = getTasks() //tasksList
-  const textBox = document.querySelector(".todo-list");
+  for (const item of items) {
+    renderItem(item, fragment);
+  }
 
-  textBox.innerHTML = "";
-  tasks.forEach(task => {
-    renderTask(task)
+  container.appendChild(fragment);
+};
+
+const renderItem = (item, fragment) => {
+  const newListItem = document.createElement("li");
+  if (item.completed) {
+    newListItem.className = "completed";
+  }
+
+  const newListDiv = document.createElement("div");
+  newListDiv.className = "view";
+
+  const newListCheckBox = document.createElement("input");
+  newListCheckBox.className = "toggle";
+  newListCheckBox.type = "checkbox";
+  newListCheckBox.checked = item.completed;
+  newListCheckBox.onclick = () => {
+    item.completed = !item.completed;
+    newListCheckBox.checked = item.completed;
+    if (item.completed) {
+      newListItem.className = "completed";
+    } else {
+      newListItem.className = "";
+    }
+    updateTask(item);
+  };
+
+  const newListItemLabel = document.createElement("label");
+  newListItemLabel.textContent = item.title;
+  newListItemLabel.addEventListener("dblclick", () => {
+    const editingItem = document.querySelector("li.editing");
+    if (editingItem) {
+      editingItem.classList.remove("editing");
+    }
+    newListItem.className = "editing";
+    const elementsToHide = document.querySelectorAll(
+      ".todo-list li:not(.editing)"
+    );
+    elementsToHide.forEach((e) => (e.style.display = "none"));
+    newEditInput.focus();
   });
-}
-/**
- * Hides an element using its class or id
- * @param {string} elementSelector 
- */
-export function hideElement(elementSelector) {
-  let elementToHide = document.querySelector(elementSelector);
-  elementToHide.classList.add("hidden");
-}
 
-/**
-* Shows a hidden element using its class or id
-*/
-export function showElement(elementSelector) {
-  let elementToShow = document.querySelector(elementSelector);
-  elementToShow.classList.remove("hidden");
-}
+  const destroyItemButton = document.createElement("button");
+  destroyItemButton.className = "destroy";
+  destroyItemButton.onclick = () => {
+    removeTask(item);
+    newListItem.remove();
+  };
 
+  newListDiv.appendChild(newListCheckBox);
+  newListDiv.appendChild(newListItemLabel);
+  newListDiv.appendChild(destroyItemButton);
 
-  /**
- * Deletes a task by id
- * @param {number} deleteTaskId 
- */
-  export function deleteTask(deleteTaskId) {
-    let tasks = getTasks();
- 
-    let listWithDeleted = tasks.filter(task => task.id != deleteTaskId);
- 
-    updateTasksList(listWithDeleted);
- }
- 
- /**
-  * Deletes all the tasks completed
-  */
- export function deleteAllTasks() {
-    let tasks = getTasks();
- 
-    let pendingTasksList = tasks.filter(task => task.completed != true);
- 
-    updateTasksList(pendingTasksList);
- }
+  const newEditInput = document.createElement("input");
+  newEditInput.className = "edit";
+  newEditInput.value = item.title;
+  newEditInput.addEventListener("keyup", (event) => {
+    if (event.key === "Escape" || event.key === "Enter") {
+      if (event.key === "Escape") {
+        newListItem.className = "";
+      } else if (event.key === "Enter") {
+        if (event.target.value) {
+          item.title = event.target.value.trim();
+          updateTask(item);
+          newListItemLabel.textContent = item.title;
+        }
+        const editingElement = document.querySelector(".editing");
+        if (item.completed) {
+          editingElement.className = "completed";
+        } else {
+          editingElement.className = "";
+        }
+      }
+      const elementsToShow = document.querySelectorAll(".todo-list li");
+      elementsToShow.forEach((e) => (e.style.display = "block"));
+    }
+  });
 
+  newListItem.appendChild(newListDiv);
+  newListItem.appendChild(newEditInput);
+
+  fragment.appendChild(newListItem);
+
+  return fragment;
+};
+
+const updateTask = (item) => {
+  const items = getStorage();
+  const newItems = items.map((i) => {
+    if (i.id === item.id) {
+      return {
+        ...i,
+        title: item.title,
+        completed: item.completed,
+      };
+    } else {
+      return i;
+    }
+  });
+  localStorage.setItem("mydayapp-js", JSON.stringify(newItems));
+  showClearButton(newItems);
+};
+
+const removeTask = (item) => {
+  const items = getStorage();
+  const newItems = items.filter((i) => i.id !== item.id);
+  localStorage.setItem("mydayapp-js", JSON.stringify(newItems));
+  if (newItems.length === 0) {
+    hideContent();
+  }
+  updateTotalTasks(newItems);
+  showClearButton(newItems);
+};
+
+const updateTotalTasks = (items) => {
+  const fragment = document.createDocumentFragment();
+  const strong = document.createElement("strong");
+  strong.textContent = items.length;
+  const fragmentText = document.createDocumentFragment();
+  fragmentText.textContent = items.length === 1 ? " item left" : " items left";
+  fragment.appendChild(strong);
+  fragment.appendChild(fragmentText);
+  const counter = document.querySelector(".todo-count");
+  counter.innerHTML = "";
+  counter.appendChild(fragment);
+};
+
+const showClearButton = (items) => {
+  const button = document.querySelector(".clear-completed");
+  const completedItems = items.filter((e) => e.completed);
+  if (completedItems.length > 0) {
+    button.style.display = "block";
+  } else {
+    button.style.display = "none";
+  }
+};
